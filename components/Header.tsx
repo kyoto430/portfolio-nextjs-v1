a81412920118a7a800eea2d48a2eb8e47f8744bf
Ruslan Prakapets <nateriver345@gmail.com>
2022-10-31 16:30:48 +0300

    add header:social icons

---
+++ components/Header.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { SocialIcon } from 'react-social-icons'
+
+type Props = {}
+
+export default function Header({}: Props) {
+  return (
+    <header className="sticky top-0 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
+      <div className="flex flex-row items-center">
+        {/* Social icons */}
+        <SocialIcon
+          url="https://instagram.com/ruslan_prakapets"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://telegram.com/@Kyoto430"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://vk.com/Kyoto430"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+      </div>
+      <div className="flex flex-row items-center text-gray-300 cursor-pointer">
+        <SocialIcon
+          className="cursor-pointer"
+          network="email"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+          Get In Touch
+        </p>
+      </div>
+    </header>
+  )
+}
